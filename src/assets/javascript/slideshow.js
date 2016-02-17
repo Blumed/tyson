0660d1310f7f730db763b53d17c261dfe94cc274
Cullan Luther <cullan.luther@gmail.com>
2016-02-16 23:21:03 -0600

    biffed share/change collections name/slick.js/started working on slideshows

--- src/assets/javascript/slideshow.js
+++ src/assets/javascript/slideshow.js
@@ -1,5 +1,5 @@
-$('.block.is-active').backstretch([
-      "http://dl.dropbox.com/u/515046/www/outside.jpg"
-    , "http://dl.dropbox.com/u/515046/www/garfield-interior.jpg"
-    , "http://dl.dropbox.com/u/515046/www/cheers.jpg"
-  ], {duration: 3000, fade: 750});
+// $('.blocks').backstretch([
+//       "http://dl.dropbox.com/u/515046/www/outside.jpg"
+//     , "http://dl.dropbox.com/u/515046/www/garfield-interior.jpg"
+//     , "http://dl.dropbox.com/u/515046/www/cheers.jpg"
+//   ], {duration: 3000, fade: 750});
